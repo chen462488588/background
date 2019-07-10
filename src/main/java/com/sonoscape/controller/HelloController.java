d40666149b03589f676d1e81304c7c90abe63a6a
chenkun <462488588@qq.com>
2019-07-10 19:37:22 +0800

    添加json web token 测试

--- src/main/java/com/sonoscape/controller/HelloController.java
+++ src/main/java/com/sonoscape/controller/HelloController.java
@@ -1,11 +1,22 @@
 package com.sonoscape.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.sonoscape.entity.Student;
 import com.sonoscape.service.IHelloService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Date;
+
 
 @Controller
 @Slf4j
@@ -24,4 +35,29 @@ public class HelloController {
         return a;
     }
 
+    //模拟登陆
+    @GetMapping("test1")
+    @ResponseBody
+    public String test(@RequestParam String name, @RequestParam String password) {
+        Key key = new SecretKeySpec("mysecret".getBytes(), SignatureAlgorithm.HS512.getJcaName());
+        Student student = new Student(name, password);
+        String subject = JSON.toJSONString(student);
+        Date exp = new Date(System.currentTimeMillis() + 2 * 60 * 1000);
+        String jws = Jwts.builder().setSubject(subject).signWith(SignatureAlgorithm.HS512, key).setExpiration(exp).compact();
+        return jws;
+    }
+
+    //模拟需要验证的接口
+    @GetMapping("test2")
+    @ResponseBody
+    public String test1(@RequestParam(required = false) String accessToken) {
+        Key key = new SecretKeySpec("mysecret".getBytes(), SignatureAlgorithm.HS512.getJcaName());
+
+        try {
+            Jwts.parser().setSigningKey(key).parseClaimsJws(accessToken);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "111";
+    }
 }
